65b6fa213ac27138bc06f341fa7a41350c9647ca
liguoqiang <331258747@qq.com>
2019-03-14 17:31:29 +0800

    1.1.2.3

--- LetsgoGuideApp/app/src/main/java/com/njz/letsgoappguides/constant/UrlConstant.java
+++ LetsgoGuideApp/app/src/main/java/com/njz/letsgoappguides/constant/UrlConstant.java
@@ -14,8 +14,8 @@ public class UrlConstant {
         if(AppUtils.getVersionCodeInt() % 100 == 0){
             return "http://www.njiuzou.com/";//外网地址
         }else{
-            return "http://192.168.100.115:9091/";//本地
-//            return "http://114.115.161.62:8088/guide-admin/";//测试地址  18357025945 654321
+//            return "http://192.168.100.115:9091/";//本地
+            return "http://114.115.161.62:8088/guide-admin/";//测试地址  18357025945 654321
 
 //            return "http://www.njiuzou.com/";//外网地址
         }
